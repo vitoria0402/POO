6d716dff98c231abb0918efb5e60e1d603d053e6
vitoria0402 <161655451+vitoria0402@users.noreply.github.com>
2024-04-03 21:21:13 -0300

    atv1.java

---
+++ atv1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Atividade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número inteiro: ");
+        int num = scanner.nextInt();
+        
+        System.out.println("tabuada de " + num + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+        scanner.close();
+    }
+}
